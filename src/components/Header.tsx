507104bac54751107824d764da78b12503d0e26c
Oakkar1998 <oakkarnyunt@gmail.com>
2025-08-09 00:09:01 +0630

    first day

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <>
+      <header className="">
+        <div className="container border-b-2 border-gray-200 py-4 item-center mx-auto">
+          <h1 className="text-3xl">Furniture</h1>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,11 +1,14 @@
+import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
 
 function RootLayout() {
   return (
     <>
-      <>RootLayout</>
-      <Outlet />
-      <footer>Footer</footer>
+      <div className="w-full min-h-screen ">
+        <Header />
+        <Outlet />
+        <footer>Footer</footer>
+      </div>
     </>
   );
 }
